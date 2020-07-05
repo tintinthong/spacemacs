0464d9aaba743b774736d154004ee0128f4d94f6
Justin <justinthong93@gmail.com>
2020-07-05 10:18:18 +0800

    Add spotify

--- init.el
+++ init.el
@@ -68,6 +68,7 @@ values."
      docker
      command-log
      search-engine
+     spotify
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
